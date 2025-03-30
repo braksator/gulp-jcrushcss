04cbfbef2c08ab95522852e7f6efa0125a14de21
braksator <braksator@users.noreply.github.com>
2025-03-30 22:25:50 +1000

    TS update

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,24 @@
-declare module 'gulp-jcrushcss' {
-  export interface JCrushCSSOptions {
-    html?: Array<string>;
-    inline?: boolean;
-    appendExt?: boolean;
-    eval?: boolean;
-    let?: boolean;
-    semi?: boolean;
-    strip?: boolean;
-    reps?: number;
-    prog?: boolean;
-    fin?: boolean;
-    maxLen?: number;
-    omit?: Array<string>;
-    clean?: boolean;
-    words?: boolean;
-    trim?: boolean;
-    break?: Array<string>;
-    split?: Array<string>;
-    escSafe?: boolean;
-  }
-
-  export function jcrushCssCode(code: string, opts?: JCrushCSSOptions): string;
-  export function jcrushCssFile(inputFile: string, outputFile: string, opts?: JCrushCSSOptions): void;
-  export function gulp(opts?: JCrushCSSOptions): any;
+export interface JCrushCSSOptions {
+  html?: string[];
+  inline?: boolean;
+  appendExt?: boolean;
+  eval?: boolean;
+  let?: boolean;
+  semi?: boolean;
+  strip?: boolean;
+  reps?: number;
+  prog?: boolean;
+  fin?: boolean;
+  maxLen?: number;
+  omit?: string[];
+  clean?: boolean;
+  words?: boolean;
+  trim?: boolean;
+  break?: string[];
+  split?: string[];
+  escSafe?: boolean;
 }
+
+declare function gulpJCrushCSS(opts?: JCrushOptions): any;
+
+export function gulpJCrushCSS(opts?: JCrushCSSOptions): any;
